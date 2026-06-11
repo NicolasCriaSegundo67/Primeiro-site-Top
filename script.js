9de6791bd1be6949059ca024fa7441b59cb3f552
NicolasCria_Segundo <nicolas.lian.lara@escola.pr.gov.br>
2026-06-11 09:03:58 -0300

    Novos botões de aumentar fonte e de alto contraste

---
+++ script.js
@@ -0,0 +1,30 @@
+ // Botão de curtidas
+const botaoCurtir = document.getElementById("curtir");
+
+botaoCurtir.addEventListener("click", () => {
+    let curtidas = document.querySelector("#curtir span");
+    curtidas.textContent = parseInt(curtidas.textContent) + 1;
+});
+
+
+
+
+// Botão A+
+const botaoTamanho = document.getElementById("tamanho");
+tamanho.addEventListener("click", aumentarFonte)
+let fonte = 16;
+
+// FUNÇÃO
+function aumentarFonte(){
+    fonte++;
+    document.body.style.fontSize = fonte + "px";
+}
+
+
+
+// Botão Contraste
+const botaoContraste = document.getElementById("contraste");
+
+botaoContraste.addEventListener("click", () => {
+    document.body.classList.toggle("alto-contraste");
+});
